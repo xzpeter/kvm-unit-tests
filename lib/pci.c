fdcda4245014664156260ca1c8664a6eda990767
Peter Xu <peterx@redhat.com>
2016-10-24 17:33:23 +0800

    pci: provide pci_scan_bars()
    Let's provide a more general way to scan PCI bars, rather than read the
    config registers every time.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- lib/pci.c
+++ lib/pci.c
@@ -206,7 +206,7 @@ static void pci_dev_print(pcidevaddr_t dev)
 	if ((header & PCI_HEADER_TYPE_MASK) != PCI_HEADER_TYPE_NORMAL)
 		return;
 
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < PCI_BAR_NUM; i++) {
 		if (pci_bar_size(&pci_dev, i)) {
 			printf("\t");
 			pci_bar_print(&pci_dev, i);
@@ -224,5 +224,16 @@ void pci_print(void)
 	for (dev = 0; dev < PCI_DEVFN_MAX; ++dev) {
 		if (pci_dev_exists(dev))
 			pci_dev_print(dev);
+	}
+}
+
+void pci_scan_bars(struct pci_dev *dev)
+{
+	int i = 0;
+
+	for (i = 0; i < PCI_BAR_NUM; i++) {
+		if (!pci_bar_is_valid(dev, i))
+			continue;
+		dev->bar[i] = pci_bar_get_addr(dev, i);
 	}
 }
